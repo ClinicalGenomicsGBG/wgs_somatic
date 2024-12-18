d1117d822383756974545f56da06a9f17362fb00
Bart-Edelbroek <bart.edelbroek@gmail.com>
2024-12-18 15:37:56 +0100

    Remove unused function

--- workflows/scripts/ascat/run_ascat.R
+++ workflows/scripts/ascat/run_ascat.R
@@ -6,20 +6,6 @@ library(tidyverse)
 library(plotly)
 library(htmlwidgets)
 
-
-data_reduce <- function(X,Y, fract){
-  len_new <- floor(length(X)/fract)
-  if(len_new <= 1){
-    stop('Not enough points for given value of fract. Reduce the value of fract!!!')
-  }
-  ind_new <- seq(1:len_new)
-  X_new <- X[fract*ind_new]
-  Y_new <- Y[fract*ind_new]
-  df_new <- data.frame(X_new, Y_new)
-  names(df_new) <- c('X','Y')
-  return(df_new)
-}
-
 # Get command line arguments
 args <- commandArgs(trailingOnly = TRUE)
 
