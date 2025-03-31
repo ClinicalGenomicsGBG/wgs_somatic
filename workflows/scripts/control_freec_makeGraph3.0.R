97d99afd65c0732c3b19941e0bafe7f623297335
Bart-Edelbroek <bart.edelbroek@gmail.com>
2025-03-31 10:45:43 +0200

    Add comments to control_freec_makeGraph3.0.R for improved readability

--- workflows/scripts/control_freec_makeGraph3.0.R
+++ workflows/scripts/control_freec_makeGraph3.0.R
@@ -1,8 +1,9 @@
-
+# Load required libraries
 library(ggpubr)
 library(dplyr)
 library(cowplot)
 
+# Function to calculate the most common increment value in a vector
 calculate_increment <- function(vector, num_rows = 5) {
   # Ensure there are enough rows to compare
   if (length(vector) < num_rows) {
@@ -16,42 +17,49 @@ calculate_increment <- function(vector, num_rows = 5) {
   return(as.numeric(names(sort(table(increments), decreasing = TRUE)[1])))
 }
 
-show_points = 5E5
-max_cutoff = 3  # Maximum ratio to plot
+# Constants
+show_points = 5E5  # Number of points to show in the BAF plot
+max_cutoff = 3     # If the ratio is greater than this value * baseline, it will be capped to this value
 
+# Parse command-line arguments
 args <- commandArgs(trailingOnly = TRUE)
-tumor_id <- args[1]
-ratio_file <- args[2]
-BAF_file <- args[3]
-fai_file <- args[4]
-cytoband <- args[5]
-output_ratio_plot <- args[6]
-output_ratio_seg <- args[7]
-output_BAF_igv <- args[8]
+tumor_id <- args[1]               # Tumor sample ID
+ratio_file <- args[2]             # File containing ratio data
+BAF_file <- args[3]               # File containing BAF data
+fai_file <- args[4]               # FAI file (genome index) used to adjust chromosome positions
+cytoband <- args[5]               # Cytoband file (chromosome banding information)
+output_ratio_plot <- args[6]      # Output file for the ratio plot
+output_ratio_seg <- args[7]       # Output file for the ratio segmentation
+output_BAF_igv <- args[8]         # Output file for the BAF IGV track
 
-# Check if the fai file exists
+# Check if the FAI file exists
 if (!file.exists(fai_file)) {
   stop(paste("FAI file not found:", fai_file))
 }
 
+# Set the default theme for plots
 theme_set(theme_pubclean())
 
+# Read the ratio data into a data frame
 ratio <- data.frame(read.table(ratio_file, header=TRUE))
 
+# Read the BAF data into a data frame
 BAF <- data.frame(read.table(BAF_file, header=TRUE))
 
+# Read the FAI file and process it
 fai <- read.table(fai_file, header = FALSE, sep = "\t", 
                   col.names = c("chr", "length", "start", "value1", "value2")) %>%
-  mutate(chr = substr(chr,4,30))%>%
-  mutate(middle = start + (length / 2))
-fai <- fai[1:24,]
+  mutate(chr = substr(chr, 4, 30)) %>%  # Remove the "chr" prefix from chromosome names
+  mutate(middle = start + (length / 2)) # Calculate the middle position of each chromosome
+fai <- fai[1:24,]  # Keep only the first 24 rows (autosomes and sex chromosomes)
 
+# Read the cytoband file and process it
 cytoband <- data.frame(read.table(cytoband, header = TRUE)) %>%
-  mutate(chrom = substr(chrom,4,30)) %>%
-  left_join(fai, by = c("chrom" = "chr")) %>%
-  mutate(adjStart = start + chromStart,
-         adjEnd = start + chromEnd,
-         color = case_when(
+  mutate(chrom = substr(chrom, 4, 30)) %>%  # Remove the "chr" prefix from chromosome names
+  left_join(fai, by = c("chrom" = "chr")) %>%  # Join with FAI data
+  mutate(adjStart = start + chromStart,       # Adjust start positions
+         adjEnd = start + chromEnd,           # Adjust end positions
+         color = case_when(                   # Assign colors based on band type
             gieStain == "acen" ~ "#C00000",
             gieStain == "gneg" ~ "#E0E0E0",
             gieStain == "gpos25" ~ "#C0C0C0",
@@ -63,36 +71,40 @@ cytoband <- data.frame(read.table(cytoband, header = TRUE)) %>%
             TRUE ~ "#FFFFFF"
          ))
 
-ploidy = median(ratio$CopyNumber[which(ratio$MedianRatio>0.8 & ratio$MedianRatio<1.2)], na.rm = T)
-cat (c("INFO: Selected ploidy:", ploidy, "\n"))
+# Calculate the ploidy based on the ratio data
+ploidy = median(ratio$CopyNumber[which(ratio$MedianRatio > 0.8 & ratio$MedianRatio < 1.2)], na.rm = TRUE)
+cat(c("INFO: Selected ploidy:", ploidy, "\n"))
 
+# Calculate the most common increment value in the ratio data
 increment <- calculate_increment(ratio$Start)
 
+# Adjust the ratio data for plotting
 ratio_adjusted <- ratio %>%
   left_join(fai, by = c("Chromosome" = "chr")) %>%
-  mutate(adjStart = Start + start,
-         End = Start + increment,
-         corr_ratio = case_when(
+  mutate(adjStart = Start + start,  # Adjust start positions
+         End = Start + increment,  # Calculate end positions
+         corr_ratio = case_when(   # Correct the ratio values
             Ratio * ploidy > max_cutoff * ploidy ~ Ratio * ploidy - (round(Ratio * ploidy) - max_cutoff * ploidy),
             TRUE ~ Ratio * ploidy),
-         CopyNumber_adj = case_when(
+         CopyNumber_adj = case_when(  # Adjust copy number values
             CopyNumber > max_cutoff * ploidy ~ max_cutoff * ploidy,
             TRUE ~ CopyNumber),
-         color = case_when(
+         color = case_when(  # Assign colors based on ratio values
             Ratio >= 0.9 & Ratio <= 1.1 ~ "around 1",
             Ratio < 0.9 & Ratio >= 0 ~ "below 0.9",
             Ratio > 1.1 ~ "above 1.1",
             TRUE ~ "other"
-         ))%>%
-  filter(Ratio > 0, BAF > 0)
+         )) %>%
+  filter(Ratio > 0, BAF > 0)  # Filter out invalid rows
 
-# Calculate breaks and labels
+# Calculate breaks and labels for the y-axis
 max_corr_ratio <- max(ratio_adjusted$corr_ratio, na.rm = TRUE)
 breaks <- seq(0, max_corr_ratio, by = 1)
 labels <- c(as.character(breaks[-length(breaks)]), paste0(">", max(breaks) - 1))
 
+# Create the ratio plot
 Ratio_plot <- ggplot(fai) +                                              
-  geom_rect(data = cytoband, aes(xmin = adjStart, xmax = adjEnd, ymin = -(max_cutoff*ploidy*0.035), ymax = -(max_cutoff*ploidy*0.01), fill = color)) +
+  geom_rect(data = cytoband, aes(xmin = adjStart, xmax = adjEnd, ymin = -(max_cutoff * ploidy * 0.035), ymax = -(max_cutoff * ploidy * 0.01), fill = color)) +
   geom_point(data = ratio_adjusted, aes(adjStart, corr_ratio, color = color), shape = '.') +
   geom_point(data = ratio_adjusted, aes(adjStart, CopyNumber_adj), shape = '.', col = "#00000050") +
   geom_vline(aes(xintercept = start), col = "grey") +
@@ -104,29 +116,33 @@ Ratio_plot <- ggplot(fai) +
   scale_fill_identity() +
   theme(legend.position = "none", axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
 
+# Adjust the BAF data for plotting
 BAF_adjusted <- BAF %>%
   left_join(fai, by = c("Chromosome" = "chr")) %>%
   mutate(adjPosition = Position + start) %>%
   filter(BAF > 0, uncertainty < 1) %>%
   select(Chromosome, Position, adjPosition, BAF, FittedA, FittedB, A, B, uncertainty)
 
-BAF_adjusted_red <- BAF_adjusted%>%
-  slice(which(row_number() %% floor(n()/show_points) == 1))
-BAF_plot <-  ggplot(fai)+                                              
-  geom_vline(aes(xintercept = start), col = "grey")+
-  geom_point(data = BAF_adjusted_red, aes(adjPosition,BAF),shape='.', col = "#00000010")+
-  geom_point(data = BAF_adjusted_red[BAF_adjusted_red$uncertainty > 0 & BAF_adjusted_red$uncertainty < 1,], aes(adjPosition,FittedA),shape=15, size = 0.2, col = "#0090FF")+
-  geom_point(data = BAF_adjusted_red[BAF_adjusted_red$uncertainty > 0 & BAF_adjusted_red$uncertainty < 1,], aes(adjPosition,FittedB),shape=15, size = 0.2, col = "#FF5050")+
+# Reduce the number of points for the BAF plot
+BAF_adjusted_red <- BAF_adjusted %>%
+  slice(which(row_number() %% floor(n() / show_points) == 1))
+
+# Create the BAF plot
+BAF_plot <- ggplot(fai) +                                              
+  geom_vline(aes(xintercept = start), col = "grey") +
+  geom_point(data = BAF_adjusted_red, aes(adjPosition, BAF), shape = '.', col = "#00000010") +
+  geom_point(data = BAF_adjusted_red[BAF_adjusted_red$uncertainty > 0 & BAF_adjusted_red$uncertainty < 1,], aes(adjPosition, FittedA), shape = 15, size = 0.2, col = "#0090FF") +
+  geom_point(data = BAF_adjusted_red[BAF_adjusted_red$uncertainty > 0 & BAF_adjusted_red$uncertainty < 1,], aes(adjPosition, FittedB), shape = 15, size = 0.2, col = "#FF5050") +
   geom_text(aes(label = chr, x = middle, y = Inf), vjust = 1, size = 3) +
-  scale_y_continuous("BAF", limits = c(0, 1), expand = expansion(mult = 0.1))+
-  theme(axis.title.x=element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
+  scale_y_continuous("BAF", limits = c(0, 1), expand = expansion(mult = 0.1)) +
+  theme(axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
 
-
+# Save the combined ratio and BAF plots as a PNG file
 png(output_ratio_plot, width = 24, height = 12, units = "cm", pointsize = 20, res = 1200)
 plot_grid(Ratio_plot, BAF_plot, ncol = 1, align = "v", rel_heights = c(5, 2))
 dev.off()
 
-
+# Write the ratio segmentation data to a file
 writeLines("#track graphType=points maxHeightPixels=300:300:300 color=0,0,0 altColor=0,0,0", con = output_ratio_seg)
 ratio_adjusted %>%
   mutate(Sample = tumor_id,
@@ -134,7 +150,7 @@ ratio_adjusted %>%
   select(Sample, Chromosome, Start, End, corr_ratio) %>%
   write.table(output_ratio_seg, sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE, append = TRUE)
 
-
+# Write the BAF data to an IGV-compatible file
 writeLines(paste0(
   '#type=GENE_EXPRESSION\n#track graphtype=points name="',
   tumor_id,
