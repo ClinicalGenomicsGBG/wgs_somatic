289eca4cd5003759c61e75679e108265569ace5b
Bart-Edelbroek <bart.edelbroek@gmail.com>
2025-04-28 13:15:22 +0200

    update TMB filter to use DP for coverage (new sentieon) instead of AFDP (old sentieon)

--- workflows/scripts/calculate_tmb.sh
+++ workflows/scripts/calculate_tmb.sh
@@ -12,13 +12,13 @@ include_normal=$8
 
 # Adjust coverage filtering based on include_normal parameter
 if [ "$include_normal" == "True" ]; then
-    filt_norm="&& FORMAT/AFDP[1]>$min_coverage"  # Include normal in coverage filter
+    filt_norm="&& FORMAT/DP[1]>$min_coverage"  # Include normal in coverage filter
 else
     filt_norm=""  # Only use the tumor sample in coverage filter
 fi
 
 # Filter, get total mutations, and calculate TMB
-$bcftools filter -i "FORMAT/AF[0]>$min_mutant_allele_fraction && FORMAT/AD[0:1]>$min_mutant_allele_reads && FORMAT/AFDP[0]>$min_coverage $filt_norm" $input_vcf | \
+$bcftools filter -i "FORMAT/AF[0]>$min_mutant_allele_fraction && FORMAT/AD[0:1]>$min_mutant_allele_reads && FORMAT/DP>$min_coverage $filt_norm" $input_vcf | \
 $bcftools stats | grep 'number of records:' | awk '{print $NF}' | \
 awk -v gsize=$genome_size '{ printf "TMB\t%.2f\n", ($1 / gsize * 1e6) }' > $output_file
 
