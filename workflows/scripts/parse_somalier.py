6e0ce8028f7bd4728dd59485c786fcb510b09575
Bart-Edelbroek <bart.edelbroek@gmail.com>
2025-11-05 17:56:19 +0100

    Add parser for somalier output for tumor-normal samples

---
+++ workflows/scripts/parse_somalier.py
@@ -0,0 +1,79 @@
+import csv
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+class SomalierParser:
+    """
+    Class for parsing a somalier run with a tumor-normal pair or a single tumor or normal sample.
+
+    Attributes:
+        relatedness (float): The (first) relatedness value from the pairs file.
+        sex (male/female/unknown): The parsed sex prediction from the normal (if available) or otherwise tumor sample.
+        y_depth_mean (float): The Y_depth_mean from the normal (if available) or otherwise tumor sample.
+    """
+
+    def __init__(
+        self, pairs_file, samples_file, tumorstring="tumor", normalstring="normal"
+    ):
+        self.relatedness = self.parse_somalier_pairs(pairs_file)
+        self.sampleid, self.sex, self.y_depth_mean = self.parse_somalier_samples(
+            samples_file, tumorstring, normalstring
+        )
+
+    def parse_somalier_pairs(self, pairs_file):
+        """
+        Parses a somalier pairs.tsv file using csv and returns the relatedness value.
+        Assumes data in first row after the header.
+        """
+        with open(pairs_file, newline="") as f:
+            reader = csv.DictReader(f, delimiter="\t")
+            row = next(reader, None)
+            if row is None:
+                logger.info("No relatedness data found in pairs file.")
+                return None
+            return float(row["relatedness"])
+
+    def parse_somalier_samples(
+        self, samples_file, tumorstring="tumor", normalstring="normal"
+    ):
+        """
+        Parses a somalier samples.tsv file using csv.
+        Returns the sex and Y_depth_mean for the normal if available or otherwise the tumor.
+        """
+        sex_map = {"1": "male", "2": "female"}
+        normal = tumor = None
+        with open(samples_file, newline="") as f:
+            reader = csv.DictReader(f, delimiter="\t")
+            for row in reader:
+                if row["sample_id"].startswith(normalstring):
+                    normal = (
+                        row["sample_id"],
+                        sex_map.get(row["sex"], "unknown"),
+                        float(row["Y_depth_mean"]),
+                    )
+                if row["sample_id"].startswith(tumorstring):
+                    tumor = (
+                        row["sample_id"],
+                        sex_map.get(row["sex"], "unknown"),
+                        float(row["Y_depth_mean"]),
+                    )
+        if normal:
+            return normal
+        elif tumor:
+            logger.warning("Normal sample not found; using tumor sample data instead.")
+            return tumor
+        else:
+            logger.error("Neither normal nor tumor data found in samples file.")
+            return None, None, None
+
+
+if __name__ == "__main__":
+    # Expect input files as arguments
+    pairs_file = sys.argv[1]
+    samples_file = sys.argv[2]
+
+    somalier = SomalierParser(pairs_file, samples_file)
+    print(somalier.sampleid, somalier.relatedness, somalier.sex)
